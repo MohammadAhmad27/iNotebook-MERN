edfea46645591295e17e307b2affbad0942d75f3
Mohammad Ahmad <ahmadabbasi5936@gmail.com>
2024-04-22 00:09:47 +0500

    Changed all fetuser to fetchUser in middleware, auth.js & notes.js

--- backend/middleware/fetchuser.js
+++ backend/middleware/fetchuser.js
@@ -1,7 +1,7 @@
 var jwt = require('jsonwebtoken');
 const JWT_SECRET = 'Harryisagoodb$oy';
 
-const fetchuser = (req, res, next) => {
+const fetchUser = (req, res, next) => {
     // Get the user from the jwt token and add id to req object
     const token = req.header('auth-token');
     if (!token) {
@@ -18,4 +18,4 @@ const fetchuser = (req, res, next) => {
 }
 
 
-module.exports = fetchuser;
+module.exports = fetchUser;
