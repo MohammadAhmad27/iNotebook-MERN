4fb23c0bf8a8bb1dc324b7484b0f419f2d3deef1
Mohammad Ahmad <ahmadabbasi5936@gmail.com>
2024-05-05 15:14:05 +0500

    Updated Form

--- src/components/AddNote.js
+++ src/components/AddNote.js
@@ -7,7 +7,7 @@ const AddNote = (props) => {
 
     const [note, setNote] = useState({ title: "", description: "", tag: "" })
 
-    const handleClick = (e) => {
+    const handleSubmit = (e) => {
         e.preventDefault();
         addNote(note.title, note.description, note.tag);
         setNote({ title: "", description: "", tag: "" });
@@ -20,7 +20,7 @@ const AddNote = (props) => {
     return (
         <div className="container my-3">
             <h2>Add a Note</h2>
-            <form className="my-3">
+            <form className="my-3" onSubmit={handleSubmit}>
                 <div className="mb-3">
                     <label htmlFor="title" className="form-label">Title</label>
                     <input type="text" className="form-control" id="title" name="title" aria-describedby="emailHelp" value={note.title} onChange={onChange} minLength={5} required />
@@ -30,7 +30,7 @@ const AddNote = (props) => {
                     <input type="text" className="form-control" id="description" name="description" value={note.description} onChange={onChange} minLength={5} required />
                 </div>
 
-                <button disabled={note.title.length < 5 || note.description.length < 5} type="submit" className="btn btn-primary" onClick={handleClick}>Add Note</button>
+                <button disabled={note.title.length < 5 || note.description.length < 5} className="btn btn-primary" >Add Note</button>
             </form>
         </div>
     )
